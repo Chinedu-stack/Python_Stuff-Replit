8c28a9205459e8cad77fdbc7cf56871046916e3a
Chinedu-stack <chineduchile123@gmail.com>
2025-09-29 21:13:23 +0100

    progress

---
+++ replit/Day54/day54-project3.py
@@ -0,0 +1,16 @@
+import csv
+### 🔹 Mini Project 3: Personal Library 📚
+### Task
+###
+### Ask the user to enter books with these details:
+### - Title
+### - Author
+### - Year
+###
+### Keep asking until the user types "stop" for the title.
+###
+### Save all the books into a CSV file: title, author, year.
+###
+### Read the CSV back in.
+###
+### Print only the books published after 2000.
