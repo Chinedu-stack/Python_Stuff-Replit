56d8f654e9715ac9d8f24dd29bc5a78d5875190e
Chinedu <chineduchile123@gmail.com>
2026-03-04 21:54:31 +0000

    js stuff

---
+++ JavaScript Lessons/lesson3/t.js
@@ -0,0 +1,4 @@
+let score = -9;
+let team = "Man U";
+let wonGame = true;
+console.log(`The team ${team} have won the game with a score of ${score} points. This statement is ${wonGame}`)
---
+++ JavaScript Lessons/lesson4/a.js
@@ -0,0 +1,9 @@
+let apples = 12;
+let oranges = 10;
+let bananas = 8;
+
+apples -= 3;
+oranges += 5;
+apples %= 8;
+
+console.log(apples, oranges, bananas);
