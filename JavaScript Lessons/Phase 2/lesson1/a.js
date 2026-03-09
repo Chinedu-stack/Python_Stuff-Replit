93d95fd9da06f9965610db959483206da9be8bbc
Chinedu <chineduchile123@gmail.com>
2026-03-09 21:58:46 +0000

    js work

---
+++ JavaScript Lessons/Phase 2/lesson1/a.js
@@ -0,0 +1,10 @@
+let numbers = [2,3,4,5,6];
+
+
+let total = 0
+for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i]
+    console.log("Calculating total...")
+}
+
+console.log(total);
---
+++ JavaScript Lessons/Phase 2/lesson2/a.js
@@ -0,0 +1,7 @@
+let animals = ["bird", "bird", "eagle", "kangaroo", "lion"];
+
+animals.splice(2, 2, "hamster", "snake");
+console.log(animals)
+
+let array = animals.slice(0, 4)
+console.log(array)
