202676134259b4e65bbe12e2560d440c7ab81ad8
Chinedu-stack <chineduchile123@gmail.com>
2026-09-01 21:34:04 +0100

    stuff

--- Learning_SQLAlchemy/lesson1.py
+++ Learning_SQLAlchemy/lesson1.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from sqlalchemy.engine import URL
 from sqlalchemy import text
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer
+from sqlalchemy.orm import Mapped, mapped_column
 
 connection_url = URL.create(
     "postgresql+psycopg",
@@ -10,15 +14,37 @@ connection_url = URL.create(
     port=5432,
     database="barber_app"
 )
-
 engine = create_engine(connection_url)
+session = Session(engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    customer_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column()
+    phone_number: Mapped[str] = mapped_column()
+
 
 with engine.connect() as connection:
-     result = connection.execute(
-        text("SELECT * FROM barbers")
-    )
 
-     row = result.fetchone()
+     
+     result = connection.execute(
+        text("""
+            SELECT * FROM customers
+            WHERE name = :name
+        """),
+         {"name": "Unique"}
+        )
+    
+
+     row = result.fetchall()
 
      print(row)
-     print(row._mapping)
+
+
+
+
